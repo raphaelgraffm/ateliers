7d78436b7da157a34cb1b93d0989a89e6775aa97
raphaelgraffm <raphgraff@live.fr>
2016-10-09 16:41:36 +0200

    Premiere implementation de l'espace membre

--- developpement/scripts/operations.sh
+++ developpement/scripts/operations.sh
@@ -3,6 +3,12 @@ echo "Creation des assets.."
 php bin/console assetic:dump
 php bin/console assetic:dump --env=prod
 
+# Pleeease
+echo "Passage par Pleeease.."
+pleeease compile
+rm -f web/css/styles.css
+mv -f web/css/styles2.css web/css/styles.css
+
 # Vidage des caches
 echo "Vidage des caches.."
 php bin/console cache:clear --env=prod
